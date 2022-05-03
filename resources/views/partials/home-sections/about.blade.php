d95e7d73f89f3af943c73fd0531940068c91bc44
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2022-05-02 22:26:43 -0400

    about section setup

--- resources/views/partials/home-sections/about.blade.php
+++ resources/views/partials/home-sections/about.blade.php
@@ -1,5 +1,12 @@
 <div class="content-wrapper" id='about'>
-    <p>about me goes here</p>
-    <p>want to know more about me?</p>
-    <a href="/erin">click here :]</a>
+    <div class="h2-wrapper">
+        <h2>
+            about erin
+        </h2>
+    </div>
+    <div class="text-wrapper">
+        <p>about me goes here</p>
+        <p>want to know more about me?</p>
+        <a href="/erin">click here :]</a>
+    </div>
 </div>
