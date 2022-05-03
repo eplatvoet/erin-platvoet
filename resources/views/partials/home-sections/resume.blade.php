b930e3e1e122015d79ee41adc373bb56bb1c016c
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2022-05-02 22:26:54 -0400

    resume section set up

--- resources/views/partials/home-sections/resume.blade.php
+++ resources/views/partials/home-sections/resume.blade.php
@@ -1,4 +1,13 @@
 <div class="content-wrapper" id='resume'>
-    <p>resume goes here</p>
-
+    <div class="h2-wrapper">
+        <h2>
+            my background
+        </h2>
+    </div>
+    <div class="text-wrapper">
+        <p>languages/tools/etc</p>
+        <p>veeva certz?</p>
+        <a href="./pdf/dummy.pdf" target="_blank">click here to see my resume</a>
+        <p>for more info, please feel free to connect via LinkedIn, or email me by <a href="mailto:erin.didomenico520@gmail.com">clicking here</a>.</p>
+    </div>
 </div>
