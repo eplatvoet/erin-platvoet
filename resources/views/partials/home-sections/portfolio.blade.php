bbaa4b19ef8d89ff941690af5c989aa264da95ad
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2022-05-02 22:27:31 -0400

    cont build of portfolio section

--- resources/views/partials/home-sections/portfolio.blade.php
+++ resources/views/partials/home-sections/portfolio.blade.php
@@ -1,17 +1,62 @@
 <div class="content-wrapper" id='portfolio'>
-    <div class="project-wrapper">
-        <div class="project">
-            <img src="" alt="">
-        </div>
-        <div class="project">
-            <img src="" alt="">
-        </div>
-        <div class="project">
-            <img src="" alt="">
-        </div>
-        <div class="project">
-            <img src="" alt="">
+    <div class="h2-wrapper">
+        <h2>
+            portfolio
+        </h2>
+    </div>
+    <div class="text-wrapper">
+        <p>	&#10031;	&#10031;	&#10031;	&#10031;</p>
+    </div>
+    <div class="bootcamp-projects">
+        <h3>
+            Rutgers Coding Bootcamp Projects
+        </h3>
+        <p>
+            Below you will find some of the projects I completed during the Full Stack Web Development Coding Bootcamp at Rutgers University in 2020.
+        </p>
+        <div class="project-wrapper">
+            <div class="project" id='pokedex'>
+                <img src="./images/home/PokedexSearch.gif" alt="">
+                <div class="expand-btn-wrapper" data-bs-toggle="modal" data-bs-target="#exampleModal" id='pokedex-project'>
+                    <img src="./images/global/info.png" alt="Click here to see more information about this project" title="Click here to see more information about this project">
+                </div>
+            </div>
+            <div class="project" id='localGrub'>
+                <img src="./images/home/LocalGrub.gif" alt="">
+                <div class="expand-btn-wrapper" data-bs-toggle="modal" data-bs-target="#exampleModal" id='localGrub-project'>
+                    <img src="./images/global/info.png" alt="Click here to see more information about this project" title="Click here to see more information about this project">
+                </div>
+            </div>
+            <div class="project" id='refresh'>
+                <img src="./images/home/Refresh.gif" alt="">
+                <div class="expand-btn-wrapper" data-bs-toggle="modal" data-bs-target="#exampleModal" id='refresh-project'>
+                    <img src="./images/global/info.png" alt="Click here to see more information about this project" title="Click here to see more information about this project">
+                </div>
+            </div>
+            <div class="project" id='weatherDash'>
+                <img src="./images/home/WeatherDashboard.gif" alt="">
+                <div class="expand-btn-wrapper" data-bs-toggle="modal" data-bs-target="#exampleModal" id='weatherDash-project'>
+                    <img src="./images/global/info.png" alt="Click here to see more information about this project" title="Click here to see more information about this project">
+                </div>
+            </div>
         </div>
     </div>
-    <p>	&#10031;	&#10031;	&#10031;	&#10031;</p>
+
+    <!-- MODAL -->
+    <div class="modal fade" id="projectModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        <!-- js will add text here -->
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="" alt="">
+                    <p><span class="bold">Description</span>: <span class='text-goes-here'><!-- js will add text here --></span></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
