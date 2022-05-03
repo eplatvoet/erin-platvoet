be8550d563d67e3e75594e90c9955906963b8479
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2022-05-02 22:26:24 -0400

    bootstrap CDN

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -6,6 +6,9 @@
 
         <title>erin platvoet</title>
 
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
         <!-- FAVICON -->
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
@@ -31,14 +34,14 @@
         <div>
         @include('partials.global.hero')
         @include('partials.global.nav')
+        
         @yield('content')
-        <!-- @include('partials.home-sections.about')
-        @include('partials.home-sections.portfolio')
-        @include('partials.home-sections.resume') -->
+
         @include('partials.global.footer')
 
         </div>
 
         <script src="/js/app.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
